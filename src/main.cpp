223836cf7fa02a72e0fcbbcf7c06eb58ab06f495
Mike <Miket9717@gmail.com>
2021-08-24 00:38:10 +0300

    added basics to makefile

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// run cmake to create this config file or do it manually.
+#include "config.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << argv[0] << endl;
+    return 0;
+}
