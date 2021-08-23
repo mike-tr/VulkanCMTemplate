31481708af7b3166606645046e463465800e59d3
Mike <Miket9717@gmail.com>
2021-08-24 00:54:45 +0300

    done with template

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
-// run cmake to create this config file or do it manually.
-#include "config.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+
 #include <iostream>
 
-using namespace std;
+int main() {
+    glfwInit();
 
-int main(int argc, char *argv[]) {
-    cout << argv[0] << endl;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
